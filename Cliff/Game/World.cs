8a5e631760027253f9362fafaf4e385771e8c242
Maximilian Kruse <maxkruse1996@gmail.com>
2018-02-04 12:47:55 +0100

    Initial Commit

---
+++ Cliff/Game/World.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cliff
+{
+    class World
+    {
+        static void Main(string[] args)
+        {
+            StartAdventure();
+        }
+
+        private static void StartAdventure()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
