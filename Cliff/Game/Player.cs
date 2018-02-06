f7969c760a0010dba9024eeadc3cd2961c5998aa
Maximilian Kruse <maxkruse1996@gmail.com>
2018-02-06 20:32:29 +0100

    Variable Changes

--- Cliff/Game/Player.cs
+++ Cliff/Game/Player.cs
@@ -2,41 +2,66 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace cliff
+namespace Cliff
 {
     class Player
     {
-        int level { get; set; }
+        private int level;
 
-        int hp_current { get; set; }
-        int hp_max { get; set; }
-        float hp_max_modifier { get; set; }
+        int hp_current;
+        int hp_max;
+        float hp_max_modifier;
 
-        int exp { get; set; }
-        float exp_modifier { get; set; }
+        int exp;
+        float exp_modifier;
 
-        int exp_Up { get; set; }
-        float exp_Up_modifier { get; set; }
+        int exp_Up;
+        float exp_Up_modifier;
 
         List<int> slots;
 
-        int attack { get; set; }
-        float attack_modifier { get; set; }
+        int attack;
+        float attack_modifier;
 
-        int defense { get; set; }
-        float defense_modifier { get; set; }
+        int defense;
+        float defense_modifier;
 
-        float critical_rate { get; set; }
-        float critical_rate_modifier { get; set; }
+        float critical_rate;
+        float critical_rate_modifier;
 
-        float critical_damage { get; set; }
-        float critical_damage_modifier { get; set; }
+        float critical_damage;
+        float critical_damage_modifier;
 
-        int speed { get; set; }
-        float speed_modifier { get; set; }
+        int speed;
+        float speed_modifier;
 
-        float speed_Gauge { get; set; }
-        int priority { get; set; }
+        float speed_Gauge;
+        int priority;
+
+
+        public int Level { get => level; set => level = value; }
+        public int Hp_current { get => hp_current; set => hp_current = value; }
+        public int Hp_max { get => hp_max; set => hp_max = value; }
+        public float Hp_max_modifier { get => hp_max_modifier; set => hp_max_modifier = value; }
+        public int Exp { get => exp; set => exp = value; }
+        public float Exp_modifier { get => exp_modifier; set => exp_modifier = value; }
+        public int Exp_Up { get => exp_Up; set => exp_Up = value; }
+        public float Exp_Up_modifier { get => exp_Up_modifier; set => exp_Up_modifier = value; }
+        public List<int> Slots { get => slots; set => slots = value; }
+        public int Attack { get => attack; set => attack = value; }
+        public float Attack_modifier { get => attack_modifier; set => attack_modifier = value; }
+        public int Defense { get => defense; set => defense = value; }
+        public float Defense_modifier { get => defense_modifier; set => defense_modifier = value; }
+        public float Critical_rate { get => critical_rate; set => critical_rate = value; }
+        public float Critical_rate_modifier { get => critical_rate_modifier; set => critical_rate_modifier = value; }
+        public float Critical_damage { get => critical_damage; set => critical_damage = value; }
+        public float Critical_damage_modifier { get => critical_damage_modifier; set => critical_damage_modifier = value; }
+        public int Speed { get => speed; set => speed = value; }
+        public float Speed_modifier { get => speed_modifier; set => speed_modifier = value; }
+        public float Speed_Gauge { get => speed_Gauge; set => speed_Gauge = value; }
+        public int Priority { get => priority; set => priority = value; }
+
+
 
         public Player(int level, int hp_current, int hp_max, float hp_max_modifier, int exp, float exp_modifier, int exp_Up, float exp_Up_modifier,
             int attack, float attack_modifier, int defense, float defense_modifier, float critical_rate, float critical_rate_modifier,
@@ -64,5 +89,8 @@ namespace cliff
             this.speed_Gauge = speed_Gauge;
             this.priority = priority;
         }
+
     }
+
+
 }
