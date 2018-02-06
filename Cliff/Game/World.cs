db40f67881437e279d3731a82f7dabab2cceec53
Maximilian Kruse <maxkruse1996@gmail.com>
2018-02-06 20:32:48 +0100

    GUI + First Testings

--- Cliff/Game/World.cs
+++ Cliff/Game/World.cs
@@ -11,7 +11,13 @@ namespace Cliff
 
         private static void StartAdventure()
         {
-            
+            Player p = new Player(1, 50, 100, 0, 0, 0, 100, 0, 15, 0, 15, 0, 15, 0, 15, 0, 30, 0, 0, 1);
+
+            while (true)
+            {
+                Utils.GUI.Draw(p);
+                Console.ReadKey();
+            }
         }
     }
 }
---
+++ Cliff/Utils/GUI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cliff.Utils
+{
+    static class GUI
+    {
+        public static void Draw(Cliff.Player p)
+        {
+            Console.WriteLine(p.Level + " - " + p.Hp_current + " - " + p.Speed);
+
+        }
+    }
+}
